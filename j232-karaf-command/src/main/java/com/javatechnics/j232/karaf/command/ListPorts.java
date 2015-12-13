765d7bc4bf2ab5dc37427a316b8d1b2826e2ff5e
Kerry Billingham <github@avionicengineers.com>
2015-12-13 16:26:47 +0000

    Included ListPorts command.
    ListPorts Karaf command class included. Currently incomplete.

---
+++ j232-karaf-command/src/main/java/com/javatechnics/j232/karaf/command/ListPorts.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javatechnics.j232.karaf.command;
+
+/**
+ *
+ * @author Kerry Billingham <contact@AvionicEngineers.com>
+ */
+public class ListPorts {
+    
+}
